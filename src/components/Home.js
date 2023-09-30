6b0e0da150971bcf4d01f56cbd0c35193f85f3c5
Aditya Raj <101439988+RajAditya01@users.noreply.github.com>
2023-09-30 11:30:11 +0530

    .

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home = () =>{
+    return (
+        <div>
+            <p>WELLCOME</p>
+            <h1>We are The Mern Developer</h1>
+        </div>
+    )
+}
+
+export default Home;
