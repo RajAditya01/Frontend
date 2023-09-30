7eff1f5122d551bac858c1f290cb7fc212f19c7a
Aditya Raj <101439988+RajAditya01@users.noreply.github.com>
2023-09-30 16:38:22 +0530

    .

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
+
+
 const App = () =>{
     return (
         <>
           <Navbar/>
-          <Home/>
-        </>
+          <Route>
+            <Home/>
+          </Route>
+
+          <Route>
+            <About/>
+          </Route>
+
+          <Route>
+            <Contact/>
+          </Route>
+
+          <Route>
+            <Login/>
+          </Route>
+
+          <Route>
+            <Signup/>
+          </Route>
+        </>        
     )
 }
 
